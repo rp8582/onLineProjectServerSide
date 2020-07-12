2f954f0eade63d02c4d97d1cb37b8486a8d0f75a
Hagit <rachelp8582@gmail.com>
2020-07-12 17:48:03 +0300

    12/07-token

---
+++ API/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        public IHttpActionResult GetToken(string name, string phone)
+        {
+            try
+            {
+                return Ok( BL.Token.GetToken(name, phone));
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ BL/Token.cs
@@ -0,0 +1,60 @@
+﻿using BL.manager;
+using BL.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Token
+    {
+        private static JWTContainerModel getJWTContainerModel(string name,string phone)
+        {
+            return new JWTContainerModel()
+            {
+                Claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,name),
+                    new Claim(ClaimTypes.MobilePhone,phone),
+                }
+            };
+        }
+
+        public static string GetToken(string name,string phone)
+        {
+            IAuthContainerModel model = getJWTContainerModel(name, phone);
+            IAuthService authService = new JWTService(model.SecretKey);
+
+            string token = authService.GenerateToken(model);
+            if (!authService.IsTokenValid(token))
+                throw new UnauthorizedAccessException();
+            else
+            {
+                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+
+                string tokenName = claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value;
+                string tokenPhone = claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.MobilePhone)).Value;
+
+            }
+            return token;
+        }
+        /*
+        public static string GetPhoneFromToken(string token)
+        {
+          
+            if (!authService.IsTokenValid(token))
+                throw new UnauthorizedAccessException();
+            else
+            {
+                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+
+                string tokenName = claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value;
+                string tokenPhone = claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.MobilePhone)).Value;
+
+            }
+        }*/
+    }
+}
